42d78d1616d664d17da1eb33cd41d1207518f95b
Vinícius Vidal <viniciusvidal.dev@gmail.com>
2026-06-16 12:35:35 -0300

    decode bencoded lists

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, fmt};
 
 use anyhow::{Context, Result, anyhow, bail};
 
@@ -6,29 +6,64 @@ use anyhow::{Context, Result, anyhow, bail};
 enum BencodeValue {
     String(Vec<u8>),
     Integer(i64),
+    List(Vec<BencodeValue>),
 }
 
-fn decode_bencoded_value(encoded_value: &str) -> Result<BencodeValue> {
-    match encoded_value.chars().next() {
-        Some('i') => {
-            let end = encoded_value.find('e').context("missing 'e' in interger")?;
-            let raw = &encoded_value[1..end];
+impl fmt::Display for BencodeValue {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            BencodeValue::Integer(i) => write!(f, "{i}"),
+            BencodeValue::String(s) => write!(f, "{}", String::from_utf8_lossy(s)),
+            BencodeValue::List(l) => {
+                writeln!(f, "[")?;
+                for (i, val) in l.iter().enumerate() {
+                    if i + 1 == l.len() {
+                        writeln!(f, "  {val}")?;
+                    } else {
+                        writeln!(f, "  {val},")?;
+                    }
+                }
+                write!(f, "]")
+            }
+        }
+    }
+}
+
+fn decode_bencoded_value(encoded_value: &str) -> Result<(BencodeValue, &str)> {
+    match encoded_value.as_bytes().first() {
+        Some(b'i') => {
+            let rest = &encoded_value[1..]; // skip leading "i"
+            let end = rest.find('e').context("missing 'e' in integer")?;
+            let raw = &rest[..end];
             if raw == "-0" {
                 bail!("invalid integer: -0 is not allowed");
             }
             let n: i64 = raw.parse().map_err(|e| anyhow!("invalid integer: {e}"))?;
-            Ok(BencodeValue::Integer(n))
+            Ok((BencodeValue::Integer(n), &rest[end + 1..])) // [end + 1..] to skip terminating "e"
         }
-
-        Some('0'..='9') => {
+        Some(b'l') => {
+            let mut rest = &encoded_value[1..]; // skip leading "l"
+            let mut values = Vec::new();
+            while !rest.starts_with('e') {
+                let (val, remaining) = decode_bencoded_value(rest)?;
+                values.push(val);
+                rest = remaining;
+            }
+            Ok((BencodeValue::List(values), &rest[1..])) // [1..] to skip terminating "e"
+        }
+        Some(b'0'..=b'9') => {
             let (len_str, rest) = encoded_value
                 .split_once(':')
                 .context("missing ':' in string")?;
             let len: usize = len_str
                 .parse()
                 .map_err(|e| anyhow!("invalid length: {e}"))?;
-            Ok(BencodeValue::String(rest[..len].as_bytes().to_vec()))
+            Ok((
+                BencodeValue::String(rest[..len].as_bytes().to_vec()),
+                &rest[len..],
+            ))
         }
+        None => bail!("unexpected end of input or empty string"),
         _ => bail!("unsupported bencode value: {}", encoded_value),
     }
 }
@@ -40,11 +75,8 @@ fn main() -> Result<()> {
     match command.as_deref() {
         Some("decode") => {
             let encoded = args.next().context("missing argument: encoded value")?;
-            let value = decode_bencoded_value(&encoded)?;
-            match value {
-                BencodeValue::Integer(i) => println!("{i}"),
-                BencodeValue::String(s) => println!("{}", String::from_utf8_lossy(&s)),
-            }
+            let (value, _) = decode_bencoded_value(&encoded)?;
+            println!("{value}");
         }
         Some(cmd) => println!("unknown command: {cmd}"),
         None => println!("no command provided"),
