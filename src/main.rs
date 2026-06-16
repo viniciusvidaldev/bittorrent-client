12e2378b0cd10c3dc35b813598e75b27b6783395
Vinícius Vidal <viniciusvidal.dev@gmail.com>
2026-06-16 16:01:01 -0300

    decode bencoded dict

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,32 @@
-use std::{env, fmt};
+use std::{collections::HashMap, env, fmt};
 
 use anyhow::{Context, Result, anyhow, bail};
 
-#[derive(Debug)]
+#[derive(PartialEq)]
 enum BencodeValue {
     String(Vec<u8>),
     Integer(i64),
     List(Vec<BencodeValue>),
+    Dict(HashMap<Vec<u8>, BencodeValue>),
 }
 
-impl fmt::Display for BencodeValue {
+impl fmt::Debug for BencodeValue {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
+            BencodeValue::String(s) => write!(f, "{:?}", String::from_utf8_lossy(s)),
             BencodeValue::Integer(i) => write!(f, "{i}"),
-            BencodeValue::String(s) => write!(f, "{}", String::from_utf8_lossy(s)),
-            BencodeValue::List(l) => {
-                writeln!(f, "[")?;
-                for (i, val) in l.iter().enumerate() {
-                    if i + 1 == l.len() {
-                        writeln!(f, "  {val}")?;
-                    } else {
-                        writeln!(f, "  {val},")?;
-                    }
-                }
-                write!(f, "]")
-            }
+            BencodeValue::List(l) => f.debug_list().entries(l).finish(),
+            BencodeValue::Dict(d) => f
+                .debug_map()
+                .entries(d.iter().map(|(k, v)| (String::from_utf8_lossy(k), v)))
+                .finish(),
         }
     }
 }
 
 fn decode_bencoded_value(encoded_value: &str) -> Result<(BencodeValue, &str)> {
-    match encoded_value.as_bytes().first() {
-        Some(b'i') => {
+    match encoded_value.chars().next() {
+        Some('i') => {
             let rest = &encoded_value[1..]; // skip leading "i"
             let end = rest.find('e').context("missing 'e' in integer")?;
             let raw = &rest[..end];
@@ -41,7 +36,7 @@ fn decode_bencoded_value(encoded_value: &str) -> Result<(BencodeValue, &str)> {
             let n: i64 = raw.parse().map_err(|e| anyhow!("invalid integer: {e}"))?;
             Ok((BencodeValue::Integer(n), &rest[end + 1..])) // [end + 1..] to skip terminating "e"
         }
-        Some(b'l') => {
+        Some('l') => {
             let mut rest = &encoded_value[1..]; // skip leading "l"
             let mut values = Vec::new();
             while !rest.starts_with('e') {
@@ -51,7 +46,24 @@ fn decode_bencoded_value(encoded_value: &str) -> Result<(BencodeValue, &str)> {
             }
             Ok((BencodeValue::List(values), &rest[1..])) // [1..] to skip terminating "e"
         }
-        Some(b'0'..=b'9') => {
+        Some('d') => {
+            let mut map = HashMap::new();
+            let mut rest = &encoded_value[1..]; // skip leading "d"
+            while !rest.starts_with('e') {
+                let (key, remaining) = decode_bencoded_value(rest)?;
+                rest = remaining;
+                let BencodeValue::String(key) = key else {
+                    bail!("dict keys must be strings, got {:?}", key);
+                };
+
+                let (val, remaining) = decode_bencoded_value(rest)?;
+                rest = remaining;
+                map.insert(key, val);
+            }
+
+            Ok((BencodeValue::Dict(map), &rest[1..])) // [1..] to skip terminating "e"
+        }
+        Some('0'..='9') => {
             let (len_str, rest) = encoded_value
                 .split_once(':')
                 .context("missing ':' in string")?;
@@ -76,7 +88,7 @@ fn main() -> Result<()> {
         Some("decode") => {
             let encoded = args.next().context("missing argument: encoded value")?;
             let (value, _) = decode_bencoded_value(&encoded)?;
-            println!("{value}");
+            println!("{value:?}");
         }
         Some(cmd) => println!("unknown command: {cmd}"),
         None => println!("no command provided"),
